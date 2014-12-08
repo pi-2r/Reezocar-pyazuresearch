abfb7cf2a90c5ed1bdc8f92231c60809f84bda5c
zen <pierre.therrode@reezocar.com>
2014-12-08 14:54:41 +0100

    add

---
+++ pyazuresearch/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+from pyazuresearch.client import AzureSearch
+
+
+
+__author__ = 'Pierre Therrode'
+__all__ = ['AzureSearch']
+__version__ = '0.1'
+__date__ = '12 November 2014'
+__credits__ = "Thanks to Reezocar -reezocar.com-"
+__version_info__ = tuple(__version__.split('.'))
+get_version = lambda: __version_info__
+
